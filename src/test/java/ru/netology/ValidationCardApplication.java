929864f475e992ecbef08380e84eef6efe8b4250
Megalapka <forvsykoe@list.ru>
2022-08-10 16:10:23 +0300

    created all tests: success and not success

--- src/test/java/ru/netology/ValidationCardApplication.java
+++ src/test/java/ru/netology/ValidationCardApplication.java
@@ -29,7 +29,6 @@ public class ValidationCardApplication {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        //driver = new ChromeDriver();
     }
 
     @AfterEach
@@ -53,6 +52,47 @@ public class ValidationCardApplication {
 
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotSuccessByFirstField() {
+        driver.get("http://localhost:9999/");
+
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("we2");
+
+        driver.findElement(By.cssSelector("div button")).click();
+
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector("span.input_has-value.input_invalid[data-test-id='name'] .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotSuccessBySecondField() {
+        driver.get("http://localhost:9999/");
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванова Анна");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+799");
+
+        driver.findElement(By.cssSelector("div button")).click();
+
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("span.input_has-value.input_invalid[data-test-id='phone'] .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotSuccessByCheckBox() {
+        driver.get("http://localhost:9999/");
+
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванова Анна");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79996644444");
+        driver.findElement(By.cssSelector("div button")).click();
+
+        String expected = "rgba(255, 92, 92, 1)";
+        String actual = driver.findElement(By.cssSelector(".input_invalid[data-test-id='agreement']")).getCssValue("color");
         assertEquals(expected, actual);
     }
 
